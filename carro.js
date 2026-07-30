6ce44ac8b9ba1f775dceaac99c4e9f6225453521
pedrolimaoliveira08-afk <pedro.lima.oliveira08@escola.pr.gov.br>
2026-07-30 11:45:18 -0300

    Create carro.js

---
+++ carro.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta name="viewport" content="width=,devide-width,initial-scale=1.0">
+    <title>Herança em java Script</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="conteiner">
+        <h1 Herança em javaScript></h1>
+        <button onclick="mostrarcarros()">mostrar dados</button>
+        <div id = "resultado"></div>
+    </div>
+    
+    <script src="Carro.js"></script>
